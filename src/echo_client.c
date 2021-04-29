9dbbeb9f7973e407eef29db30050c4a2909effd7
KorolTung <loveus3512_korol@163.com>
2021-04-29 11:24:06 +0800

    ch06

--- src/echo_client.c
+++ src/echo_client.c
@@ -5,46 +5,45 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define BUF_ _SIZE 1024
-void error_ handling(char *message);
+#define BUF_SIZE 1024
+void error_handling(char *message);
 int main(int argc, char *argv[])
 {
 
 	int sock;
-	char message[BUF_ SIZE];
-	int str_ _len;
-	struct sockaddr_ in serv_ adr;
+	char message[BUF_SIZE];
+	int str_len;
+	struct sockaddr_in serv_adr;
 	if(argc!=3) {
 	printf("Usage : %s <IP> <port>\n", argv[0]);
 	exit(1);
 	}
-	sock=socket(PF_ INET, SOCK_ _STREAM, 0);
+	sock=socket(PF_INET, SOCK_STREAM, 0);
 	if(sock==-1)
-	error_ handling(" socket() error");
-	memset(&serv_ adr, 0, sizeof(serv_ adr));
-	serv_ adr .sin_ family=AF _INET;
-	serv_ adr. sin_ addr.s_ addr=inet _addr(argv[1]);
-	serv_ _adr. sin_ port=htons (atoi (argv[2]));
-	if(connect(sock, (struct sockaddr*)&serv_ adr, sizeof(serv_ adr))==-1)
-	error_ handling("connect() error!");
+	error_handling(" socket() error");
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;                                                                                                                                                                                                                                                          .sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_adr.sin_port=htons (atoi (argv[2]));
+	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+	error_handling("connect() error!");
 	else
 	puts("Connecte..........");
 	while(1){
-	
-	fputs("Input message(Q to quit): "£¬stdout);
-	fgets(message, BUF_ SIZE, stdin);
-	if(!strcmp(message, "q\n") II !strcmp(message , "Q\n"))
-	break;£¬
+	fputs("Input message(Q to quit): ", stdout);
+	fgets(message, BUF_SIZE, stdin);
+	if(!strcmp(message, "q\n") || !strcmp(message , "Q\n"))
+	break;
 	write(sock, message, strlen(message));
-	str_ len=read(sock, message, BUF_ SIZE-1);
-	message[str_ 1en]=0;
-	printf( "Message from server: %s"£¬message);
+	str_len=read(sock, message, BUF_SIZE-1);
+	message[str_len]=0;
+	printf("Message from server: %s", message);
 	}
 	close(sock);
 	return 0;
 	
 }
-	void error_ handling( char *message)
+	void error_handling( char *message)
 	{
 		fputs(message, stderr);
 		fputc('\n', stderr);
--- src/echo_server.c
+++ src/echo_server.c
@@ -3,51 +3,51 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/ socket .h>
-#define BUF_ SIZE 1024
-void error_ handling( char *message);
+#include <sys/socket.h>
+#define BUF_SIZE 1024
+void error_handling( char *message);
 int main(int argc, char *argv[])
 {
-	int serv_ sock, clnt_ sock;
-	char message[BUF_ _SIZE] ;
-	int str_ len, i;
-	struct sockaddr_ in serv_ adr, clnt_ adr;
-	socklen_ t clnt_ _adr_ _Sz;
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE] ;
+	int str_len, i;
+	struct sockaddr_in serv_adr, clnt_adr;
+	socklen_t clnt_adr_sz;
 	if(argc!=2) {
 	printf("Usage : %s <port>\n", argv[0]);
 	exit(1);
 	}
-	serv_ sock=socket(PF_ _INET, SOCK_ STREAM, 0);
-	if(serv_ sock==-1)
-	error_ handling(" socket() error");
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	if(serv_sock==-1)
+	error_handling(" socket() error");
 
-	memset(&serv_ adr, 0, sizeof(serv_ adr));
-	serv_ adr. sin_ family=AF_ INET;
-	serv_ _adr. sin_ _addr.s_ addr=hton1(INADDR_ ANY);
-	serv_ adr. sin_ port=htons (atoi(argv[1]));
-	if(bind(serv_ sock, (struct sockaddr* )&serv_ adr, sizeof(serv_ adr))=-1)
-	error_ handling("bind() error");
-	if(listen(serv_ sock, 5)==-1)
-	error_ handling("listen() error");
-	clnt_ _adr_ sz=sizeof(clnt_ adr);
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr. sin_port=htons (atoi(argv[1]));
+	if(bind(serv_sock, (struct sockaddr* )&serv_adr, sizeof(serv_adr))==-1)
+	error_handling("bind() error");
+	if(listen(serv_sock, 5)==-1)
+	error_handling("listen() error");
+	clnt_adr_sz=sizeof(clnt_adr);
 	
 	
-	for(i=0; i<5; 1++)
+	for(i=0; i<5; i++)
 	{
-	clnt_ sock=accept(serv_ sock, (struct sockaddr*)&c1nt_ adr, &clnt_ adr. _sZ);
-	if(clnt_ sock==-1)
-	error_ handling("accept() error");
+	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+	if(clnt_sock==-1)
+	error_handling("accept() error");
 	else
 	printf("Connected client %d \n", i+1);
-	while((str_ len=read(c1nt_ sock, message, BUF_ _SIZE))!=0)
-	write(c1nt_ sock, message, str. _len);
-	close(c1nt_ sock) ;
+	while((str_len=read(clnt_sock, message, BUF_SIZE))!=0)
+	write(clnt_sock, message, str_len);
+	close(clnt_sock) ;
 	}
-	close(serv_ sock);
+	close(serv_sock);
 	return 0;
 }
 	
-void error_ handling(char *message)
+void error_handling(char *message)
 {
 fputs (message, stderr);
 fputc('\n', stderr);
